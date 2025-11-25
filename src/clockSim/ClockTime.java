d82ba9a2f9accebf83866a29e42646666a138a31
uriapete <uriarteangelo@gmail.com>
2025-11-25 10:50:19 -0800

    CT - new member unixSeconds

--- src/clockSim/ClockTime.java
+++ src/clockSim/ClockTime.java
@@ -9,5 +9,8 @@ package clockSim;
  * Data Type that handles conversion between Unix integer and Hours:Minutes:Seconds.
  */
 public class ClockTime {
-
+    /**
+     * Number of seconds stored by this clock since midnight.
+     */
+    private int unixSeconds;
 }
