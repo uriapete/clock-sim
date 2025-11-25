fba6a2e1d068a1428f5dcf86b6a3a6dec58b3956
uriapete <uriarteangelo@gmail.com>
2025-11-24 19:52:19 -0800

    C - top comment

--- src/clockSim/Clock.java
+++ src/clockSim/Clock.java
@@ -4,7 +4,10 @@
 package clockSim;
 
 /**
- * 
+ * Base class for all Clocks. Contains:
+ * Integer clockTime,
+ * double driftPerSecond,
+ * virtual (empty) methods reset, tick, displayTime.
  */
 abstract class Clock {
 
