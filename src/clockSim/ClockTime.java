d69e5a2376586c3f17556020bb14a319c4e995e5
uriapete <uriarteangelo@gmail.com>
2025-11-25 10:53:33 -0800

    CT - take listTime, stringTime from Clock

--- src/clockSim/ClockTime.java
+++ src/clockSim/ClockTime.java
@@ -30,4 +30,57 @@ public class ClockTime {
      * Amount of seconds in a minute.
      */
     protected final int secsInMin = 60;
+
+    /**
+     * Returns the clock time as an array.
+     * @return The clock time as an array, syled at [Seconds, Minutes, Hours]
+     */
+    protected int[] listTime(){
+        // set up return list.
+        ArrayList<Integer> timeList = new ArrayList<>(3);
+
+        // loop time back if after 24 hours
+        int dayTime = unixSeconds % secsInDay;
+
+        // add seconds, minutes, hours
+        timeList.add(dayTime%secsInMin);
+        timeList.add(dayTime/secsInMin);
+        timeList.add(dayTime/secsInHour);
+        
+        // convert to true array
+        int[] timeArr = new int[3];
+        for (int i = 0; i < timeList.size(); i++) {
+            Integer tm = timeList.get(i);
+            timeArr[i]=tm;
+        }
+
+        // return true array
+        return timeArr;
+    }
+
+    /**
+     * Formats the clock time as a string.
+     * @return The string that represents the clock time in the format HH:MM:SS
+     */
+    protected String stringTime(){
+        // get the time in array form.
+        int[] timeArr = listTime();
+
+        // prepare return string.
+        String timeStr = "";
+
+        // for each value in array (going in order: hour, minute, second)
+        for(int i = timeArr.length-1;i>=0;--i){
+            // add the formatted string (if string is less than 2 chars, pad with 0s)
+            timeStr+=String.format("%02d", timeArr[i]);
+
+            // if this is not the end of string, add ":"
+            if (i>0) {
+                timeStr+=":";
+            }
+        }
+
+        // return string
+        return timeStr;
+    }
 }
