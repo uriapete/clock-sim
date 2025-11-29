5b6d3c1a1a47f5bd3fe2b951c8ff7c50238d3715
uriapete <uriarteangelo@gmail.com>
2025-11-29 14:47:15 -0800

    src/clockSim/DigitalClock.java - update jdoc

--- src/clockSim/DigitalClock.java
+++ src/clockSim/DigitalClock.java
@@ -3,8 +3,8 @@ package clockSim;
 /**
  * Mid level class which represents set of digital clocks.
  * @author Luis Martinez
- * @version 0.1.0.0 (MARKETING.BREAKING.FEATURE.PATCH) - 
- * Create class
+ * @version 0.1.1.0 (MARKETING.BREAKING.FEATURE.PATCH) - 
+ * Extends Clock, new constructor
  */
 public abstract class DigitalClock extends Clock {
 
