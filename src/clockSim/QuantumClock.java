7243d5d997e94abb97ecf35b19474877cb60225b
uriapete <uriarteangelo@gmail.com>
2025-11-29 14:58:06 -0800

    src/clockSim/QuantumClock.java - add jdoc

--- src/clockSim/QuantumClock.java
+++ src/clockSim/QuantumClock.java
@@ -1,5 +1,11 @@
 package clockSim;
 
+/**
+ * Mid level class which represents set of digital clocks.
+ * @author Luis Martinez
+ * @version 0.1.0.0 (MARKETING.BREAKING.FEATURE.PATCH) - 
+ * Create class
+ */
 public abstract class QuantumClock {
 
 }
