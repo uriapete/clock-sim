78616b9ca5944ceaec77e7ea4609fae833729c1b
uriapete <uriarteangelo@gmail.com>
2025-11-29 11:24:07 -0800

    CT - properly format top javadoc

--- src/clockSim/ClockTime.java
+++ src/clockSim/ClockTime.java
@@ -6,9 +6,9 @@ package clockSim;
 import java.util.ArrayList;
 
 /**
+ * Data Type that handles conversion between Unix integer and Hours:Minutes:Seconds.
  * @author Peter Uriarte
  * @version 0.1.0.0 (Epoch SemVer)
- * Data Type that handles conversion between Unix integer and Hours:Minutes:Seconds.
  */
 public class ClockTime {
     /**
