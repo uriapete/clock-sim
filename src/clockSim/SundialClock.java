7cab12f0103d83d873c93953387e2fcb4c415bf4
uriapete <uriarteangelo@gmail.com>
2025-11-29 15:46:59 -0800

    src/clockSim/SundialClock.java - add jdoc

--- src/clockSim/SundialClock.java
+++ src/clockSim/SundialClock.java
@@ -1,5 +1,10 @@
 package clockSim;
 
+/**
+ * Class that represents a sundial.
+ * @author Luis Martinez
+ * @version 0.1.0.0 - Create class
+ */
 public class SundialClock extends NaturalClock {
 
 }
