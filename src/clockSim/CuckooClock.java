52efb02f83fb271b4f2457226a92a3b071a53b4c
uriapete <uriarteangelo@gmail.com>
2025-11-30 14:38:28 -0800

    src/clockSim/CuckooClock.java - update jdoc version

--- src/clockSim/CuckooClock.java
+++ src/clockSim/CuckooClock.java
@@ -3,7 +3,7 @@ package clockSim;
 /**
  * Class that represents a Cuckoo Clock. (Cuckoo! Cuckoo!)
  * @author Luis Martinez
- * @version 0.1.0.0 (marketing.BREAKING.Features.patch) - create class
+ * @version 0.1.1.0 (marketing.BREAKING.Features.patch) - implement abstracts
  */
 public class CuckooClock extends MechanicalClock {
 
