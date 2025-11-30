72c6cd3e2995bb2bc855f72a5940d102a9ebea4f
uriapete <uriarteangelo@gmail.com>
2025-11-30 12:37:06 -0800

    src/clockSim/ClockSimulator.java - top jdoc

--- src/clockSim/ClockSimulator.java
+++ src/clockSim/ClockSimulator.java
@@ -4,7 +4,9 @@
 package clockSim;
 
 /**
- * 
+ * A program that simulates workflow of multiple types of clocks for a week and compares how much they drift from real time.
+ * @author Peter Uriarte
+ * @version 0.1.0.0 (MARKETING.BREAKING.FEATURE.PATCH) - Create class
  */
 public class ClockSimulator {
 
