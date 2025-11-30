ce8209444b59386f8e28a9b80df308be138850c0
uriapete <uriarteangelo@gmail.com>
2025-11-30 12:34:25 -0800

    NEW src/clockSim/ClockSimulator.java

---
+++ src/clockSim/ClockSimulator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package clockSim;
+
+/**
+ * 
+ */
+public class ClockSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
