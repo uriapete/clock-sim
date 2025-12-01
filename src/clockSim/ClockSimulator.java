679972c68665fd5c5aca8a5daaeb0b8b787eec2f
uriapete <uriarteangelo@gmail.com>
2025-11-30 19:57:05 -0800

    src/clockSim/ClockSimulator.java - remove todo

--- src/clockSim/ClockSimulator.java
+++ src/clockSim/ClockSimulator.java
@@ -14,8 +14,6 @@ public class ClockSimulator {
 	 * Creates a collection of different clocks, and compares their drift from creation to a week later.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 		// how many seconds in a week?
 		int secsInWeek = 604800;
 
